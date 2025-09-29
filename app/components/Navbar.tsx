58a50745025d31d67d1cce26c24f2cf96f8bf7a3
Kartik Mistry <kartikmistry301@gmail.com>
2025-09-29 07:40:47 +0530

    Changed logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,8 +4,7 @@ export const Navbar = () => {
     return (
         <nav className="w-full h-14 lg:max-w-xl max-w-[95%] bg-[#edededb8] backdrop-blur-md fixed top-6 md:pl-5 md:pr-2 px-3 md:left-[50%] left-1/2 translate-x-[-50%] rounded-full flex flex-row justify-center items-center z-[999]">
             <span className="w-full h-full flex flex-row items-center justify-start">
-                <h3 className="text-base font-black font-serif bg-white p-2 rounded-full h-6 w-6 flex items-center justify-center logo">G</h3>
-                <h4 className="text-base font-mono  font-black ml-2">GitRoll</h4>
+                <h4 className="text-base font-mono  font-black ml-2">omnis.farm</h4>
             </span>
 
             <span className="w-full h-full flex flex-row items-center justify-end md:gap-5 gap-2 md:text-base text-sm font-medium">
